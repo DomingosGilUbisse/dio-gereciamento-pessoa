5338a92069db63bff1b7bdc3aa02c42576433bd5
DomingosGilUbisse <ubissedomingosgil@gmail.com>
2021-07-18 13:18:48 +0200

    commit de criacao do projecto

---
+++ src/main/java/domingos/co/mz/diop/DioGereciamentoPessoaApplication.java
@@ -0,0 +1,13 @@
+package domingos.co.mz.diop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioGereciamentoPessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioGereciamentoPessoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/domingos/co/mz/diop/DioGereciamentoPessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package domingos.co.mz.diop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioGereciamentoPessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
